92837824933a10ca23a641700f830653c5d2dfb8
sharif-github <107545661+sharif-github@users.noreply.github.com>
2023-12-09 00:45:57 +0600

    show details button implemented

--- js/app.js
+++ js/app.js
@@ -28,15 +28,15 @@ const displayPhones = (noOfPhones) =>{
         phoneDiv.innerHTML = `
             <div class="card p-4">
                 <div class="bg-info bg-opacity-10 h-90vh rounded rounded-3 d-flex justify-content-center align-items-center">
-                <div class="w-75 h-75">
-                    <img class="rounded rounded-4 w-100 h-100" src="${phone.image}" class="card-img-top" alt="...">
-                </div>
+                    <div class="w-75 h-75">
+                        <img class="rounded rounded-4 w-100 h-100" src="${phone.image}" class="card-img-top" alt="...">
+                    </div>
                 </div>
                 <div class="card-body text-center">
                     <h4 class="card-title fw-bold">${phone.phone_name}</h4>
                     <p class="card-text text-secondary">There are many variations of passages of available, but the majority have suffered</p>
                     <h4 class="fw-bold">$999</h4>
-                    <button id="btn-search" class="btn btn-primary px-4 py-3 mt-2 fw-semibold">Show Details</button>
+                    <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary px-4 py-3 mt-2 fw-semibold" data-bs-toggle="modal" data-bs-target="#btn-show-details">Show Details</button>
                 </div>
             </div>
         `;
@@ -80,4 +80,35 @@ function toggleSpinner(isLoading){
 document.getElementById('btn-show-all').addEventListener('click', function(){
     displayPhones('all');
 })
+// Show details button handle=================
+async function loadPhoneDetails(id){
+    // console.log(id);
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const specification = await res.json();
+    showPhoneDetails(specification);
+}
+function showPhoneDetails(specification){
+    const phoneTitle = document.getElementById('btn-show-detailsLabel');
+    phoneTitle.innerText = specification.data.name;
+
+    document.getElementById('modalImage').src = specification.data.image;
+
+    document.getElementById('storage-element').innerText = specification.data.mainFeatures.storage;
+
+    document.getElementById('display-size-element').innerText = specification.data.mainFeatures.displaySize;
+
+    document.getElementById('chipset-element').innerText = specification.data.mainFeatures.chipSet;
+
+    document.getElementById('memory-element').innerText = specification.data.mainFeatures.memory;
+
+    document.getElementById('sensors-element').innerText = specification.data.mainFeatures.sensors;
+
+    document.getElementById('brand-element').innerText = specification.data.brand;
+
+    document.getElementById('gps-element').innerText = specification.data.others.GPS;
+
+    document.getElementById('date-element').innerText = specification.data.releaseDate;
+
+}
 loadPhones('iphone');
